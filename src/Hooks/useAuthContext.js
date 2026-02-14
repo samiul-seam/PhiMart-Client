9acd637f332d04d98c19f49aba0e6511b0cfabcd
samiul haque <mdsamiulhaque682@gmail.com>
2026-02-14 23:50:10 +0600

    Fix: Force case sensitivity for useAuth hook

--- src/Hooks/useAuthContext.js
+++ src/Hooks/useAuthContext.js
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import AuthContext from "../context/AuthContext.jsx";
+import AuthContext from "../context/AuthContext";
 
 const useAuthContext = () => {
   return useContext(AuthContext);
--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,5 +1,5 @@
 import { createContext } from "react";
-import useAuth from "../hooks/useAuth.js";
+import useAuth from "../hooks/useAuth";
 
 const AuthContext = createContext();
 
